ad599aab209c932fa6bbd222e64f06ad14904863
lwj <1198275198@qq.com>
2016-11-15 12:57:14 +0800

    [UPDATE]benchmark:stringConcat add another concat method

--- benchmark/stringConcat.js
+++ benchmark/stringConcat.js
@@ -15,12 +15,18 @@ var concat2 = function(str1, str2){
 
     return arr.join();
 };
+var concat3 = function(str1, str2){
+    return str1.concat(str2);
+};
 
 suite.add('concat#+', function(){
     concat1('string1', 'string2');
 })
 .add('concat#array-jion', function(){
     concat2('string1', 'string2');
+})
+.add('concat#string-concat', function(){
+    concat3('string1', 'string2');
 })
 .on('cycle', function(e){
     console.log(String(e.target));
@@ -31,7 +37,8 @@ suite.add('concat#+', function(){
 .run({'async': true});
 
 /**
- * concat#+ x 85,717,921 ops/sec ±2.47% (75 runs sampled)
-concat#array-jion x 11,301,427 ops/sec ±1.18% (79 runs sampled)
-Fastest is concat#+
+ * concat#+ x 90,483,047 ops/sec ±2.06% (84 runs sampled)
+ * concat#array-jion x 12,303,912 ops/sec ±0.90% (82 runs sampled)
+ * concat#string-concat x 40,845,196 ops/sec ±0.83% (89 runs sampled)
+ * Fastest is concat#+
  */
